c1d67b817a9967e622533a90a40835bee532220a
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-20 11:02:03 +0200

    remove questions and update reqblocked

--- Plans/parse.py
+++ Plans/parse.py
@@ -687,177 +687,177 @@ for name, app in combinedfree.items():
   # Handle Questions.yaml
   
   
-  with open("./example/questions1-no-portal.yaml", "r") as f:
-      questions1noportal = f.readlines()
-  with open("./example/questions1-portal.yaml", "r") as f:
-      questions1portal = f.readlines()
-  with open("./example/questions2.yaml", "r") as f:
-      questions2 = f.readlines()
-  with open("./example/questions2-service.yaml", "r") as f:
-      questions2service = f.readlines()
-  with open("./example/questions3.yaml", "r") as f:
-      questions3 = f.readlines()
-  with open("./example/questions4.yaml", "r") as f:
-      questions4 = f.readlines()
-  with open("./example/questions4-persistence.yaml", "r") as f:
-      questions4persistence = f.readlines()
-  with open("./example/questions5.yaml", "r") as f:
-      questions5 = f.readlines()
-  with open("./example/questions6.yaml", "r") as f:
-      questions6 = f.readlines()
-  with open("./example/questions7.yaml", "r") as f:
-      questions7 = f.readlines()
-  with open("./example/questions-env.yaml", "r") as f:
-      questionsenv = f.readlines()
-  with open("./example/questions-env-fixed.yaml", "r") as f:
-      questionsenvfixed = f.readlines()
-  with open("./export/"+"app/"+tmpname+"/questions.yaml", "w") as f:
-      if not appvaluesyaml["service"]["main"]["enabled"]:
-        for line in questions1noportal:
-          f.write(line)
-      else:
-        for line in questions1portal:
-          f.write(line)
-      f.write("\n")
-      if len(valuesyaml["env"]) != 0:
-        for line in questionsenvfixed:
-          f.write(line)
-        f.write("\n")
-        for name, value in appvaluesyaml["env"].items():
-          for line in questionsenv:
-            if "PLACEHOLDERNAME" in line:
-              f.write("        - variable: "+name+"\n")
-            elif "PLACEHOLDERLABEL" in line:
-              f.write("          label: '"+name+"'\n")
-            elif "PLACEHOLDERDESCRIPTION" in line:
-              for name2, value2  in app["Config"]["Variable"].items():
-                if value2["Target"] == name and "Description" in  value2.keys() and value2["Description"]:
-                  try:
-                    desc = value2["Description"]
-                    desc = desc.replace("\r\n", '')
-                    desc = desc.replace("\n", '')
-                    desc = desc.replace("'", '')
-                    for char in invalidtext:
-                      desc = desc.replace(char, '')
-                    desc = desc.encode("utf-8").decode("utf-8")
-                    f.write('          description: "'+desc+'"\n')
-                  except:
-                    pass
-                  break
-            elif "PLACEHOLDERVALUE" in line:
-              f.write('            default: "'+value+'"\n')
-            else:
-              f.write(line)
-      f.write("\n")
-      if appvaluesyaml["service"]["main"]["enabled"]:
-        for line in questions2:
-          if "PLACEHOLDERPORTPORT" in line:
-            f.write("                              default: "+str(valuesyaml["service"]["main"]["ports"]["main"]["port"])+"\n")
-          elif "PLACEHOLDERPORTMODE" in line:
-            f.write("                                    default: "+str(valuesyaml["service"]["main"]["ports"]["main"]["protocol"])+"\n")
-          elif "PLACEHOLDERPORTTARGET" in line:
-            f.write("                                    default: "+str(valuesyaml["service"]["main"]["ports"]["main"]["targetPort"])+"\n")
-          else:
-            f.write(line)
-        f.write("\n")
-        for name, value in appvaluesyaml["service"].items():
-          if name != "main":
-            for line in questions2service:
-              if "PLACEHOLDERSVCNAME" in line:
-                f.write("        - variable: "+name+"\n")
-              elif "PLACEHOLDERSVCLABEL" in line:
-                f.write("          label: '"+name+" service'\n")
-              elif "PLACEHOLDERDESCRIPTION" in line:
-                for name2, value2  in app["Config"]["Port"].items():
-                  if name2 == name and "Description" in  value2.keys() and value2["Description"]:
-                    try:
-                      desc = value2["Description"]
-                      desc = desc.replace("\r\n", '')
-                      desc = desc.replace("\n", '')
-                      for char in invalidtext:
-                        desc = desc.replace(char, '')
-                      desc = desc.replace("'", '')
-                      desc = desc.encode("utf-8").decode("utf-8")
-                      f.write('                      description: "'+desc+'"\n')
-                    except:
-                      pass
-                    break
-              elif "PLACEHOLDERPORTNAME" in line:
-                f.write("                    - variable: "+name+"\n")
-              elif "PLACEHOLDERPORTLABEL" in line:
-                f.write('                      label: "'+name+' Service Port Configuration"\n')
-              elif "PLACEHOLDERPORTPORT" in line:
-                f.write("                              default: "+str(valuesyaml["service"][name]["ports"][name]["port"])+"\n")
-              elif "PLACEHOLDERPORTMODE" in line:
-                f.write("                                    default: "+str(valuesyaml["service"][name]["ports"][name]["protocol"])+"\n")
-              elif "PLACEHOLDERPORTTARGET" in line:
-                f.write("                                    default: "+str(valuesyaml["service"][name]["ports"][name]["targetPort"])+"\n")
-              else:
-                f.write(line)
-        f.write("\n")
-      f.write("\n")
-      for line in questions3:
-        f.write(line)
-      f.write("\n")
-      if len(valuesyaml["persistence"]) != 0:
-        for line in questions4:
-          f.write(line)
-        f.write("\n")
-        for name, value in appvaluesyaml["persistence"].items():
-          for line in questions4persistence:
-            if "PLACEHOLDERDESCRIPTION" in line:
-              for name2, value2  in app["Config"]["Path"].items():
-                    if name2 == name and "Description" in  value2.keys() and value2["Description"]:
-                      try:
-                        desc = value2["Description"]
-                        desc = desc.replace("\r\n", '')
-                        desc = desc.replace("\n", '')
-                        for char in invalidtext:
-                          desc = desc.replace(char, '')
-                        desc = desc.replace("'", '')
-                        desc = desc.encode("utf-8").decode("utf-8")
-                        f.write('          description: "'+desc+'"\n')
-                      except:
-                        pass
-                      break
-            elif "PLACEHOLDERVAR" in line:
-              f.write("        - variable: "+name+"\n")
-            elif "PLACEHOLDERLABEL" in line:
-              f.write('          label: "'+name+' Storage"\n')
-            else:
-              f.write(line)
-        f.write("\n")
-      f.write("\n")
-      for line in questions5:
-        f.write(line)
-      f.write("\n")
-      if appvaluesyaml["service"]["main"]["enabled"]:
-        for line in questions6:
-          f.write(line)
-      f.write("\n")
-      for line in questions7:
-        if puidflag and "runAsGroupExample" in line:
-          f.write("            default: 0\n")
-        elif puidflag and  "runAsUserExample" in line:
-          f.write("            default: 0\n")
-        elif privflag and  "privilegedExample" in line:
-          f.write("                  default: true\n")
-        elif puidflag and  "runAsNonRootExample" in line:
-          f.write("                  default: false\n")
-        elif puidflag and  "readOnlyRootFilesystemExample" in line:
-          f.write("                  default: false\n")
-        elif "runAsGroupExample" in line:
-          f.write("            default: 568\n")
-        elif "runAsUserExample" in line:
-          f.write("            default: 568\n")
-        elif "privilegedExample" in line:
-          f.write("                  default: false\n")
-        elif "runAsNonRootExample" in line:
-          f.write("                  default: true\n")
-        elif "readOnlyRootFilesystemExample" in line:
-          f.write("                  default: true\n")
-        else:
-          f.write(line)
-      f.write("\n")
+  #with open("./example/questions1-no-portal.yaml", "r") as f:
+  #    questions1noportal = f.readlines()
+  #with open("./example/questions1-portal.yaml", "r") as f:
+  #    questions1portal = f.readlines()
+  #with open("./example/questions2.yaml", "r") as f:
+  #    questions2 = f.readlines()
+  #with open("./example/questions2-service.yaml", "r") as f:
+  #    questions2service = f.readlines()
+  #with open("./example/questions3.yaml", "r") as f:
+  #    questions3 = f.readlines()
+  #with open("./example/questions4.yaml", "r") as f:
+  #    questions4 = f.readlines()
+  #with open("./example/questions4-persistence.yaml", "r") as f:
+  #    questions4persistence = f.readlines()
+  #with open("./example/questions5.yaml", "r") as f:
+  #    questions5 = f.readlines()
+  #with open("./example/questions6.yaml", "r") as f:
+  #    questions6 = f.readlines()
+  #with open("./example/questions7.yaml", "r") as f:
+  #    questions7 = f.readlines()
+  #with open("./example/questions-env.yaml", "r") as f:
+  #    questionsenv = f.readlines()
+  #with open("./example/questions-env-fixed.yaml", "r") as f:
+  #    questionsenvfixed = f.readlines()
+  #with open("./export/"+"app/"+tmpname+"/questions.yaml", "w") as f:
+  #    if not appvaluesyaml["service"]["main"]["enabled"]:
+  #      for line in questions1noportal:
+  #        f.write(line)
+  #    else:
+  #      for line in questions1portal:
+  #        f.write(line)
+  #    f.write("\n")
+  #    if len(valuesyaml["env"]) != 0:
+  #      for line in questionsenvfixed:
+  #        f.write(line)
+  #      f.write("\n")
+  #      for name, value in appvaluesyaml["env"].items():
+  #        for line in questionsenv:
+  #          if "PLACEHOLDERNAME" in line:
+  #            f.write("        - variable: "+name+"\n")
+  #          elif "PLACEHOLDERLABEL" in line:
+  #            f.write("          label: '"+name+"'\n")
+  #          elif "PLACEHOLDERDESCRIPTION" in line:
+  #            for name2, value2  in app["Config"]["Variable"].items():
+  #              if value2["Target"] == name and "Description" in  value2.keys() and value2["Description"]:
+  #                try:
+  #                  desc = value2["Description"]
+  #                  desc = desc.replace("\r\n", '')
+  #                  desc = desc.replace("\n", '')
+  #                  desc = desc.replace("'", '')
+  #                  for char in invalidtext:
+  #                    desc = desc.replace(char, '')
+  #                  desc = desc.encode("utf-8").decode("utf-8")
+  #                  f.write('          description: "'+desc+'"\n')
+  #                except:
+  #                  pass
+  #                break
+  #          elif "PLACEHOLDERVALUE" in line:
+  #            f.write('            default: "'+value+'"\n')
+  #          else:
+  #            f.write(line)
+  #    f.write("\n")
+  #    if appvaluesyaml["service"]["main"]["enabled"]:
+  #      for line in questions2:
+  #        if "PLACEHOLDERPORTPORT" in line:
+  #          f.write("                              default: "+str(valuesyaml["service"]["main"]["ports"]["main"]["port"])+"\n")
+  #        elif "PLACEHOLDERPORTMODE" in line:
+  #          f.write("                                    default: "+str(valuesyaml["service"]["main"]["ports"]["main"]["protocol"])+"\n")
+  #        elif "PLACEHOLDERPORTTARGET" in line:
+  #          f.write("                                    default: "+str(valuesyaml["service"]["main"]["ports"]["main"]["targetPort"])+"\n")
+  #        else:
+  #          f.write(line)
+  #      f.write("\n")
+  #      for name, value in appvaluesyaml["service"].items():
+  #        if name != "main":
+  #          for line in questions2service:
+  #            if "PLACEHOLDERSVCNAME" in line:
+  #              f.write("        - variable: "+name+"\n")
+  #            elif "PLACEHOLDERSVCLABEL" in line:
+  #              f.write("          label: '"+name+" service'\n")
+  #            elif "PLACEHOLDERDESCRIPTION" in line:
+  #              for name2, value2  in app["Config"]["Port"].items():
+  #                if name2 == name and "Description" in  value2.keys() and value2["Description"]:
+  #                  try:
+  #                    desc = value2["Description"]
+  #                    desc = desc.replace("\r\n", '')
+  #                    desc = desc.replace("\n", '')
+  #                    for char in invalidtext:
+  #                      desc = desc.replace(char, '')
+  #                    desc = desc.replace("'", '')
+  #                    desc = desc.encode("utf-8").decode("utf-8")
+  #                    f.write('                      description: "'+desc+'"\n')
+  #                  except:
+  #                    pass
+  #                  break
+  #            elif "PLACEHOLDERPORTNAME" in line:
+  #              f.write("                    - variable: "+name+"\n")
+  #            elif "PLACEHOLDERPORTLABEL" in line:
+  #              f.write('                      label: "'+name+' Service Port Configuration"\n')
+  #            elif "PLACEHOLDERPORTPORT" in line:
+  #              f.write("                              default: "+str(valuesyaml["service"][name]["ports"][name]["port"])+"\n")
+  #            elif "PLACEHOLDERPORTMODE" in line:
+  #              f.write("                                    default: "+str(valuesyaml["service"][name]["ports"][name]["protocol"])+"\n")
+  #            elif "PLACEHOLDERPORTTARGET" in line:
+  #              f.write("                                    default: "+str(valuesyaml["service"][name]["ports"][name]["targetPort"])+"\n")
+  #            else:
+  #              f.write(line)
+  #      f.write("\n")
+  #    f.write("\n")
+  #    for line in questions3:
+  #      f.write(line)
+  #    f.write("\n")
+  #    if len(valuesyaml["persistence"]) != 0:
+  #      for line in questions4:
+  #        f.write(line)
+  #      f.write("\n")
+  #      for name, value in appvaluesyaml["persistence"].items():
+  #        for line in questions4persistence:
+  #          if "PLACEHOLDERDESCRIPTION" in line:
+  #            for name2, value2  in app["Config"]["Path"].items():
+  #                  if name2 == name and "Description" in  value2.keys() and value2["Description"]:
+  #                    try:
+  #                      desc = value2["Description"]
+  #                      desc = desc.replace("\r\n", '')
+  #                      desc = desc.replace("\n", '')
+  #                      for char in invalidtext:
+  #                        desc = desc.replace(char, '')
+  #                      desc = desc.replace("'", '')
+  #                      desc = desc.encode("utf-8").decode("utf-8")
+  #                      f.write('          description: "'+desc+'"\n')
+  #                    except:
+  #                      pass
+  #                    break
+  #          elif "PLACEHOLDERVAR" in line:
+  #            f.write("        - variable: "+name+"\n")
+  #          elif "PLACEHOLDERLABEL" in line:
+  #            f.write('          label: "'+name+' Storage"\n')
+  #          else:
+  #            f.write(line)
+  #      f.write("\n")
+  #    f.write("\n")
+  #    for line in questions5:
+  #      f.write(line)
+  #    f.write("\n")
+  #    if appvaluesyaml["service"]["main"]["enabled"]:
+  #      for line in questions6:
+  #        f.write(line)
+  #    f.write("\n")
+  #    for line in questions7:
+  #      if puidflag and "runAsGroupExample" in line:
+  #        f.write("            default: 0\n")
+  #      elif puidflag and  "runAsUserExample" in line:
+  #        f.write("            default: 0\n")
+  #      elif privflag and  "privilegedExample" in line:
+  #        f.write("                  default: true\n")
+  #      elif puidflag and  "runAsNonRootExample" in line:
+  #        f.write("                  default: false\n")
+  #      elif puidflag and  "readOnlyRootFilesystemExample" in line:
+  #        f.write("                  default: false\n")
+  #      elif "runAsGroupExample" in line:
+  #        f.write("            default: 568\n")
+  #      elif "runAsUserExample" in line:
+  #        f.write("            default: 568\n")
+  #      elif "privilegedExample" in line:
+  #        f.write("                  default: false\n")
+  #      elif "runAsNonRootExample" in line:
+  #        f.write("                  default: true\n")
+  #      elif "readOnlyRootFilesystemExample" in line:
+  #        f.write("                  default: true\n")
+  #      else:
+  #        f.write(line)
+  #    f.write("\n")
   appvaluesyaml = {}
   
